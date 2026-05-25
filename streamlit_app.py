c5f3adaf48cb7b2d67e2e715a5cbc672a120901e
Azmikousar <123876713+Azmikousar@users.noreply.github.com>
2026-05-25 12:13:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,29 @@
 import streamlit as st
+import pandas as pd
+from datetime import datetime
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.set_page_config(
+    page_title="Tez Connect",
+    page_icon="🚀",
+    layout="wide"
 )
+# -----------------------------
+# Sidebar
+# -----------------------------
+
+st.sidebar.title("🚀 Tez Connect")
+
+menu = st.sidebar.radio(
+    "Navigation",
+    [
+        "Home",
+        "Business Directory",
+        "Marketplace",
+        "Post Requirement",
+        "AI Business Assistant",
+        "Profile"
+    ]
+)
+
+
+
